d7d4dad67ea7a580ef2d3174f731cae4bcfcd78a
ritizzzz <kandelritiz@gmail.com>
2022-08-29 10:10:39 +1000

    pubsub added

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log('hello world');
+const  eventEmit = (function(){
+    let _events = {};
+    
+    const publish = function (type, callBack){
+        if(_events[type]){
+            _events[type].push = callBack;
+        }else{
+            _events[type] = [callBack];
+        }
+    };
+
+    
+    const trigger = function (type, ...args){
+        if(_events[type]){
+            _events[type].forEach(callback => {
+                callback(...args);
+            })
+        }   
+    }
+
+    return {publish, trigger};
+})();
