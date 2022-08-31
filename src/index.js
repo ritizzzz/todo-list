a5177ebd1fcc9219f28b13c06118cd547f1d9f7b
ritizzzz <kandelritiz@gmail.com>
2022-08-31 21:56:49 +1000

    todo list and add todolist todo list display has been created and add todo display has also been created

--- src/index.js
+++ src/index.js
@@ -21,3 +21,7 @@ const  eventEmit = (function(){
 
     return {subscribe, trigger};
 })();
+
+document.querySelector('.expandTodo').addEventListener('click', ()=>{
+    document.querySelector('.todo').style.height = '400px';
+})
